df2466a09fd608e31759617d4b8f7592ea769d5b
Thomas Freeborough <thomas@hotsnapper.com>
2017-08-18 09:13:17 +0100

    [User] Add actions and reducers to set user data

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -9,15 +9,27 @@ const InitialState = Record({
   data: null
 });
 
-const signIn = (state, action) =>
+const signIn = (state, action) => {
+
   state
     .set("token", action.payload.token)
     .set("data", fromJS(action.payload.data))
     .set("authenticated", true);
+};
+
+const setUserData = (state, action) => {
+  console.log('SETTING USER TO LOGGED IN');
+  state
+    .set("token", action.payload.token)
+    .set("data", fromJS(action.payload.data))
+    .set("authenticated", true);
+};
+
 
 export default handleActions(
-  {
+  { 
     [actions.user.signIn.success]: signIn,
+    [actions.user.userData.set]: setUserData,
     [actions.user.edit.success]: (state, { payload: { user } }) =>
       state.set("data", fromJS(user))
   },
